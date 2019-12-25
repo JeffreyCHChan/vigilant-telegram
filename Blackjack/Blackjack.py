637fc9e834b03193988a9b4e842d2f4c22dd5f98
Quikol <chan.jch19@gmail.com>
2019-12-24 19:52:12 -0500

    GUI enabled blackjack
    Signed-off-by: Quikol <chan.jch19@gmail.com>

--- Blackjack/Blackjack.py
+++ Blackjack/Blackjack.py
@@ -1,9 +1,7 @@
 from Functions import *
-
-
 anonymous = ["A", "a"]
 hit = ['Hit', 'hit', "H", "h"]
 stay = ['Stay', 'stay', "S", "s"]
 
-intro()
+"Intro has been discontinued at this moment "
 blackjack()
--- Blackjack/Functions.py
+++ Blackjack/Functions.py
@@ -1,6 +1,9 @@
 import random
 import numpy as np
 from UserInteraction import *
+from tkinter import *
+import os
+
 
 no_list = ["no", "n", "N", "No"]
 yes_list = ["yes", "y", "Y", "Yes"]
@@ -8,26 +11,18 @@ yes_list = ["yes", "y", "Y", "Yes"]
 
 
 Player = User(500, 0, 0, "Jim")
-def intro():
-    anonymous = ["A", "a"]
-    try:
-        name = input("What's your name.")
-        first_letter = name[0].upper()
-        rest_of_name = name[1:len(name)].lower()
-        name = first_letter + rest_of_name
-        Default = User(500, 0, 0, name)
-        print('Hello ' + Default.name)
-        print("Cash: %d, Wins: %d, Losses:%d" % (Default.Money, Default.Wins, Default.Losses))
-    except IndexError:
-        print("No name was entered, if you would like to play anonymously enter A or a")
-        name = input("What is your name?")
-        if name in anonymous:
-            print("Welcome Stranger!")
-
-
 
 def blackjack():
+    def hitClick():
+        P_random_card()
+        sum(player_cards)
+        if sum(player_cards)>21:
+            end()
+        gui()
 
+    def stayClick():
+        sum(player_cards)
+        Dealer()
     #used_card = []
     player_cards = []
     dealer_cards = []
@@ -61,132 +56,189 @@ def blackjack():
 
         #return used_card.append(First_card,Second_card)
         #return maybe for when counting cards to append to used_cards
-    def Hit_or_miss():
-        cycle = True
-        while cycle == True:
-            hit = ['Hit', 'hit', "H", "h"]
-            stay = ['Stay', 'stay', "S", "s"]
-            if np.sum(player_cards) <= 21:
-                next_move=input("Hit or Stay?")
-                if next_move in hit:
-                    P_random_card()
-                    print (player_cards)
-                    print(sum(player_cards))
-                elif next_move in stay:
-                    print (player_cards)
-                    print(sum(player_cards))
-                    return cycle == False
-
-            if np.sum(player_cards) <= 21:
-                next_move = input("Hit or Stay?")
-                if next_move in hit:
-                    P_random_card()
-                    print("\n\nYour cards\n" + str(player_cards))
-                    print(sum(player_cards))
-                elif next_move in stay:
-                    print("\n\nYour cards\n" + str(player_cards))
-                    print(sum(player_cards))
-                    return cycle == False
-            elif player_cards[0] + player_cards[1] + player_cards[2] > 21:
-                return cycle == False
-
-            if sum(player_cards) <= 21:
-                next_move = input("Hit or Stay?")
-                if next_move in hit:
-                    P_random_card()
-                    print("\n\nYour cards\n" + str(player_cards))
-                    print(sum(player_cards))
-                    if sum(player_cards)> 21:
-                        break
-
-                elif next_move in stay:
-                    print("\n\nYour cards\n" + str(player_cards))
-                    print(sum(player_cards))
-                    return cycle == False
-            elif sum(player_cards) > 21:
-                return cycle == False
-
-            if sum(player_cards) <= 21:
-                next_move = input("Hit or Stay?")
-                if next_move in hit:
-                    P_random_card()
-                    print("\n\nYour cards\n" + str(player_cards))
-                    print(sum(player_cards))
-                    if sum(player_cards) > 21:
-                        break
-                elif next_move in stay:
-                    print("\n\nYour cards\n" + str(player_cards))
-                    print(sum(player_cards))
-                    return cycle == False
-            elif sum(player_cards) > 21:
-                return cycle == False
 
     def Dealer():
-
-        D_random_card()
         cycle = True
-        #while cycle == True:
-        if sum(dealer_cards) < 17:
-            D_random_card()
-        else:
-            #print(sum(dealer_cards))
-            return cycle == False
+        while cycle == True:
+            if sum(dealer_cards) < 17:
+                D_random_card()
+            else:
+                cycle == False
+                end()
 
-        if sum(dealer_cards) < 17:
-            D_random_card()
-        else:
-            #print(sum(dealer_cards))
-            return cycle == False
 
-        if sum(dealer_cards) < 17:
-            D_random_card()
-        else:
-            #print(sum(dealer_cards))
-            return cycle == False
+    root = Tk()
+    root.geometry("500x200")
 
-        if sum(dealer_cards) < 17:
-            D_random_card()
-        else:
-            #print(sum(dealer_cards))
-            return cycle == False
+
+
+
 
     def replay():
-        replay = input("Would you like to play again?")
-        if replay in yes_list:
-            print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nCash: %d, Wins: %d, Losses:%d" % (Player.Money, Player.Wins, Player.Losses))
+        def exitReplay():
+            print("Too Bad !")
+            quit()
+
+        def restart():
+            replayTk.destroy()
             blackjack()
-        else:
-            print("Too bad :(")
+        replayTk = Tk()
+        replayTk.geometry("200x200+100+300")
+        replayLabel = Label(replayTk, text = "Replay Again")
+        replayYes = Button(replayTk, text = "Yes", command=restart)
+        replayNo = Button(replayTk, text="No", command=exitReplay)
+        replayLabel.pack()
+        replayYes.pack()
+        replayNo.pack()
+        replayTk.mainloop()
+        replayTk.quit()
+        #blackjack()
+
+
 
     def end():
         if sum(dealer_cards) < sum(player_cards) and sum(player_cards) <= 21:
-            Player.Money+= 1
-            Player.Wins+=1
-            print("\n\n\n" + "Player Wins\nYour cards\n"+str(player_cards) +"\n" + str(sum(player_cards)))
-            print('Dealer\'s cards\n'+str(dealer_cards) +"\n" + str(sum(dealer_cards)))
+            sit = Tk()
+            sit.geometry("300x200+300+300")
+
+            dealerLabel = Label(sit, text='Dealer\'s Cards')
+            dealCards = Entry(sit)
+            for x in dealer_cards:
+                dealCards.insert(INSERT, str(x) + "    ")
+            playerLabel = Label(sit, text="Player\'s Cards")
+            playerCards = Entry(sit)
+            for x in player_cards:
+                playerCards.insert(INSERT, str(x) + "    ")
+            dealerLabel.grid(row=0, column=0)
+            dealCards.grid(row=1, column=0)
+            playerLabel.grid(row=3, column=3)
+            playerCards.grid(row=4, column=3)
+
+            WinText=Label(sit, text="Player wins!")
+
+            WinText.grid(row=4,column=0)
+            root.destroy()
+            replay()
+            sit.mainloop()
+            sit.destroy()
+
+
         elif sum(player_cards) < sum(dealer_cards) and sum(dealer_cards)<=21:
-            Player.Money-= 1
-            Player.Losses += 1
-            print("\n\n\n" + "Dealer Wins!\n" + str(dealer_cards) +"\n"+ str(sum(dealer_cards)))
-            print("Your cards\n"+str(player_cards) +"\n" + str(sum(player_cards)))
+
+            sit = Tk()
+            sit.geometry("300x200+300+300")
+
+            dealerLabel = Label(sit, text='Dealer\'s Cards')
+            dealCards = Entry(sit)
+            for x in dealer_cards:
+                dealCards.insert(INSERT, str(x) + "    ")
+
+            playerLabel = Label(sit, text="Player\'s Cards")
+            playerCards = Entry(sit)
+            for x in player_cards:
+                playerCards.insert(INSERT, str(x) + "    ")
+            dealerLabel.grid(row=0, column=0)
+            dealCards.grid(row=1, column=0)
+            playerLabel.grid(row=3, column=3)
+            playerCards.grid(row=4, column=3)
+
+            WinText=Label(sit,text="Dealer wins")
+            WinText.grid(row=4,column=0)
+            root.destroy()
+            replay()
+            sit.mainloop()
+            sit.destroy()
+
         elif sum(player_cards) > sum(dealer_cards) and sum(dealer_cards) <= 21 :
-            Player.Money -= 1
-            Player.Losses += 1
-            print("\n\n\n" + "Dealer Wins!\n" + str(dealer_cards) +"\n" + str(sum(dealer_cards)))
-            print("Your cards\n"+str(player_cards) +"\n" + str(sum(player_cards)))
+            sit = Tk()
+            sit.geometry("300x200+300+300")
+
+            dealerLabel = Label(sit, text='Dealer\'s Cards')
+            dealCards = Entry(sit)
+            for x in dealer_cards:
+                dealCards.insert(INSERT, str(x) + "    ")
+            playerLabel = Label(sit, text="Player\'s Cards")
+            playerCards = Entry(sit)
+            for x in player_cards:
+                playerCards.insert(INSERT, str(x) + "    ")
+            dealerLabel.grid(row=0, column=0)
+            dealCards.grid(row=1, column=0)
+            playerLabel.grid(row=3, column=3)
+            playerCards.grid(row=4, column=3)
+
+            WinText=Label(sit,text="Dealer wins")
+            WinText.grid(row=4,column=0)
+            root.destroy()
+            replay()
+            sit.mainloop()
+            sit.destroy()
         elif sum(player_cards) == sum(dealer_cards) and sum(player_cards)<=21 and sum(dealer_cards)<=21:
-            print("Draw!")
+            sit = Tk()
+            sit.geometry("300x200+300+300")
+
+            dealerLabel = Label(sit, text='Dealer\'s Cards')
+            dealCards = Entry(sit)
+            for x in dealer_cards:
+                dealCards.insert(INSERT, str(x) + "    ")
+            playerLabel = Label(sit, text="Player\'s Cards")
+            playerCards = Entry(sit)
+            for x in player_cards:
+                playerCards.insert(INSERT, str(x) + "    ")
+            dealerLabel.grid(row=0, column=0)
+            dealCards.grid(row=1, column=0)
+            playerLabel.grid(row=3, column=3)
+            playerCards.grid(row=4, column=3)
+
+            WinText=Label(sit,text="Draw")
+            WinText.grid(row=4,column=0)
+            root.destroy()
+            replay()
+            sit.mainloop()
+            sit.destroy()
         elif sum(player_cards) < sum(dealer_cards) and sum(player_cards) <=21:
-            Player.Money += 1
-            Player.Wins += 1
-            print("\n\n\n" + "Player Wins\nYour cards\n"+str(player_cards) +"\n" + str(sum(player_cards)))
-            print('Dealer\'s cards\n'+str(dealer_cards) +"\n" + str(sum(dealer_cards)))
+            sit = Tk()
+            sit.geometry("300x200+300+300")
+
+            dealerLabel = Label(sit, text='Dealer\'s Cards')
+            dealCards = Entry(sit)
+            for x in dealer_cards:
+                dealCards.insert(INSERT, str(x) + "    ")
+            playerLabel = Label(sit, text="Player\'s Cards")
+            playerCards = Entry(sit)
+            for x in player_cards:
+                playerCards.insert(INSERT, str(x) + "    ")
+            dealerLabel.grid(row=0, column=0)
+            dealCards.grid(row=1, column=0)
+            playerLabel.grid(row=3, column=3)
+            playerCards.grid(row=4, column=3)
+
+            WinText=Label(sit,text="Player wins!")
+            WinText.grid(row=4,column=0)
+            root.destroy()
+            replay()
+            sit.mainloop()
+            sit.destroy()
         else:
             print("Something went wrong")
 
 
+    def gui():
+        dealerLabel = Label(root, text='Dealer\'s Cards')
+        dealCards = Entry(root)
+        dealCards.insert(ANCHOR, str(dealer_cards[0]))
+        playerLabel = Label(root, text="Player\'s Cards")
+        playerCards = Entry(root)
+        for x in player_cards:
+            playerCards.insert(INSERT, str(x)+"    ")
+        hitButton = Button(root, text="Hit", padx=20, command=hitClick)
+        stayButton = Button(root, text="Stay", padx=20, command=stayClick)
+        dealerLabel.grid(row=0, column=0)
+        dealCards.grid(row=1, column=0)
+        playerLabel.grid(row=3, column=3)
+        playerCards.grid(row=4, column=3)
+        hitButton.grid(row=4, column=1)
+        stayButton.grid(row=4, column=2)
+        root.mainloop()
+
     initial()
-    Hit_or_miss()
-    Dealer()
-    end()
-    replay()
+    gui()
